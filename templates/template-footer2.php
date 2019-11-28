3a70d596d829ce3557533cc05fc5c5838dde1e5b
EC2 Default User <ec2-user@ip-172-31-30-250.eu-west-1.compute.internal>
2019-11-28 11:56:23 +0000

    tercer commit

---
+++ templates/template-footer2.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: footer2
+ * Template Post Type: post, page
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ */
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+<h1>ESTO ES UN ATRACO, NO QUE VA , ES UNA PRUEBA DEL FOOTER2</h1>
+<?php
+get_footer("raro");
